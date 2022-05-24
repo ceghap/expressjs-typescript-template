7d29485c3d590b2bfd788998cb91c3dd1ba03452
Ashraf Latif <hello@ceghap.com>
2022-05-24 17:46:19 +0800

    add prettier config

--- index.ts
+++ index.ts
@@ -1,21 +1,23 @@
-import express from "express";
-import mongoose from "mongoose";
-import bodyParser from "body-parser";
-import routes from "./src/routes/crmRoutes";
-import "dotenv/config";
+import 'dotenv/config';
+
+import bodyParser from 'body-parser';
+import express from 'express';
+import mongoose from 'mongoose';
+
+import routes from './src/routes/crmRoutes';
 
 const app = express();
 const PORT: number = 3000;
 
-async function loadDb() {
+const connectDb = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
   } catch (err) {
     console.log(err);
   }
-}
+};
 
-loadDb();
+connectDb();
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,9 +26,9 @@ app.use(bodyParser.json());
 routes(app);
 
 // serving static files
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.get("/", (req, res) =>
+app.get('/', (req, res) =>
   res.send(`Node and express server is running on port ${PORT}`)
 );
 
